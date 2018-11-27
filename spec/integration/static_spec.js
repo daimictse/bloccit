9a3553682af357305686c9fb32f10988e7a0c6fa
daimictse <daimictse@gmail.com>
2018-11-26 20:54:27 -0800

    routing-controller-view-checkpoint completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -10,13 +10,10 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 //#2 making sure requesting the server using this route was successful = code 200
-    it("should return status code 200", (done) => {
-
-//#3 send a GET request to the base URL http://localhost:3000/
-// All request making methods take a function as a second argument which will
-// contain the response from the server as well as content and any errors
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
+  it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+    request.get(base, (err, res, body) => {
+       expect(res.statusCode).toBe(200);
+       expect(body).toContain("Welcome to Bloccit");
 
 //#4 Need to call done here because expect() won't be made before the test finishes
 // If no expect(), Jasmine assumes the test is successful
